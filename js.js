6e45436790dd4648e5cc3a03999a5b6171875376
srini88 <pvsrinivas.ece@gmail.com>
2016-05-02 13:33:56 -0500

    put on prototype

--- js.js
+++ js.js
@@ -4,15 +4,18 @@ var Task = function(name){
 
 	this.name = name;
 	this.completed = false;
-	this.complete = function(){
-		console.log("completing task: " + this.name);
-		this.completed = true;
-	}
-	this.save = function(){  //not on prototype..so many compies of the same godammn function
-		console.log("saving task " +  this.name);
-	}
-	//return this;  - happens by default when using new
+	
 }
+
+Task.prototype.complete = function(){   //all tasks have a link to this complete..but those tasks dont own the complete....only one copy of complete exists..
+	console.log("completing task: " + this.name);
+	this.completed = true;
+}
+
+Task.prototype.save = function(){
+		console.log("saving task " +  this.name);
+	}	
+
 
 var task1 = new Task("task1");
 var task2 = new Task("task2");
@@ -24,13 +27,15 @@ task1.save();
 task2.complete();
 task2.save();
 
+///this way it is much more efficient...
 
 ///drawbacks of this...
 
 // complete and save is recreated for every task object...all 2 times..not very efficient..
 
 
-
+////prototype shit
+////Task has a Task.prototype --- the fns complete and save are truly liked back to prototype..
 
 
 
