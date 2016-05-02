100e8c9568fd95b93a52ea20d671725f0d0b1203
srini88 <pvsrinivas.ece@gmail.com>
2016-05-02 12:54:20 -0500

    inheritance violationg writa, conf, enu

--- js.js
+++ js.js
@@ -12,18 +12,25 @@ Object.defineProperty (task4, 'toString', {
 	"value" : function(){
 		return this.title  + " ** " + this.desc;
 	}, 
-	"writable" : true, 
-	"enumerable" : true, 
-	"configurable" : true   //does not matter to use ""  or not to 
+	"writable" : false, 
+	"enumerable" : false, 
+	"configurable" : false   //does not matter to use ""  or not to 
 
 });
-
-///why you do this shit ...
-
-//task4.toString = "hi;"  //it is broken now the toString() now...
-
-//that's why you use defineProperty....to make it writable false..
+////create an urgent Taks from task 
 
 
-console.log(task4.toString());
-console.log(task4)
+var urgentTask = Object.create(task4);
+
+//see everything is set to false in task4...however urgent task inherited shit..
+Object.defineProperty (urgentTask, 'toString', {
+
+	"value" : function(){
+		return this.title  + " ** " +"urgentTask";
+	}, 
+	"writable" : false, 
+	"enumerable" : false, 
+	"configurable" : false   //does not matter to use ""  or not to 
+
+});
+console.log(urgentTask.toString());  //my task ** urgentTask
