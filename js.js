dde0908e812fea6857e4fd96382f20003b7daefe
srini88 <pvsrinivas.ece@gmail.com>
2016-06-21 20:37:35 -0500

    now with functions

--- js.js
+++ js.js
@@ -1,38 +1,18 @@
-//you know when dealing with functions you put stuff on the prototype right...same way now you dealing wiht plain object...same we we do prototype..by using Object.create() 
-
-//put stuff on the prototype and make others extend it...
+///now same shit as previous but by using functions....
 
 
-var AnswerPrototype = {
-
-	get:function fn1(){
-		return this.val;
-	}
-};
-
-
-var lifeAnswer = Object.create(AnswerPrototype);
-
-lifeAnswer.val = 42;
-
-console.log(lifeAnswer.get())
-
-var deathAnswer = Object.create(AnswerPrototype);
-
-deathAnswer.val  = 3.14;
-console.log(deathAnswer.get());  //3.14
-
-
-///others inherit FirmAnswerPrototype
-var FirmAnswerPrototype = Object.create(AnswerPrototype);
-///another way ...call the parent get method using call 
-///below this will be for lucky...
-FirmAnswerPrototype.get = function fn2(){
-	//console.log(this === luckyAnswer)  //true
-	//console.log(luckyAnswer)
-	return AnswerPrototype.get.call(this) + "!!";
+function Answer(value){
+	this._val = value;
 }
 
-var luckyAnswer = Object.create(FirmAnswerPrototype);
-luckyAnswer.val = 7;
-console.log(luckyAnswer.get())  ///7
+///put stuff on prototype...alll Instances of Answer will have access to this rptotype...
+Answer.prototype.get = function fn1(){
+	return this._val;
+}
+
+var lifeAnswer = new Answer(42);
+
+console.log(lifeAnswer.get())   //42
+
+
+console.log(Answer.prototype === Object.getPrototypeOf(lifeAnswer))  ///true
