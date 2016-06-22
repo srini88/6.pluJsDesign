deccbd6895e3fb6878e018142652c9522ea5c697
srini88 <pvsrinivas.ece@gmail.com>
2016-06-21 20:15:16 -0500

    wrong

--- js.js
+++ js.js
@@ -8,12 +8,15 @@ var answer = {
 var firmAnswer = Object.create(answer);
 
 firmAnswer.get = function fn2(){
-	return this.val +"!!";
+	return answer.get() +"!!";   //we want to call answer.get() in the context of firmAnswer  ///we are not doing it nowsd
 }
 firmAnswer.val = 3.14;
 
 
-console.log(firmAnswer.get())  //3.14
+console.log(firmAnswer.get())  //gives 42!!! which is wrong ..we want it to show 3.14!!
+
+
+
 
 
 //you got duplicated logic....in answer.get and firmAnswer.get... 
