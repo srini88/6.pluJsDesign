a91490025014577ee645056310c330a0a35d9f58
srini88 <pvsrinivas.ece@gmail.com>
2016-06-21 20:13:30 -0500

    correct

--- js.js
+++ js.js
@@ -10,11 +10,12 @@ var firmAnswer = Object.create(answer);
 firmAnswer.get = function fn2(){
 	return this.val +"!!";
 }
+firmAnswer.val = 3.14;
 
 
-console.log(answer.get())  //42
-console.log(firmAnswer.get())  //42!!
+console.log(firmAnswer.get())  //3.14
 
 
 //you got duplicated logic....in answer.get and firmAnswer.get... 
 
+///typically what you want to do is call function one from function2..
